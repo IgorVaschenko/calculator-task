737c97446386e39bf6f4e8f6b5d559e0d49e2169
Igor <103171728+IgorVaschenko@users.noreply.github.com>
2022-08-09 11:08:52 +0300

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
-    host: '172.16.220.174',
+    localhost: '172.16.220.174',
     port: 3030,
     hot: true,
     inline: true,
